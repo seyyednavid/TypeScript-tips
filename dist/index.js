3ee4a6a64b0d4736783bcf635cc7228d73ede092
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-09-22 17:46:13 +0100

    definition of class and object in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,19 @@
 "use strict";
-let phone = document.getElementById("phone");
-phone.value;
+class Account {
+    constructor(id, name, balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0) {
+            throw new Error("invalid amount");
+        }
+        this.balance += amount;
+    }
+}
+let account = new Account(1, 'David', 0);
+account.deposit(100);
+console.log(typeof account);
+console.log(account instanceof Account);
 //# sourceMappingURL=index.js.map
