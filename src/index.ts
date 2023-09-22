910d52cfce1471e39b488389383cd08caf837c13
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-09-22 19:17:15 +0100

    index signature property

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,14 @@
-// The second way for work with private properties and methods
-// Getter and Setter
+// Dynamic property
 
-class Account {
-  constructor(
-    public readonly id: number,
-    public name: string,
-    private _balance: number,
-    public nickname?: string
-  ) {}
-
-  deposit(amount: number): void {
-    if (amount <= 0) {
-      throw new Error("invalid amount");
-    }
-    this._balance += amount;
-  }
-
-  get balance(): number {
-    // with adding get we can read balance property indirectly console.log(account.balance)
-    return this._balance;
-  }
-  set balance(value: number) {
-    // with adding set we can set balance property's value indirectly => account.balance = 1
-    if (value < 0) throw new Error("Invalid value");
-    this._balance = value;
-  }
-
-  private calculateTax() {}
+//Sometimes we need to have dynamic objects, in default
+//typescript do not allow to create dynamic objects
+// we should use index signature property
+class SeatAssignment {
+  [seatNumber: string]: string;
 }
 
-let account = new Account(1, "David", 0);
-console.log(account.balance); // get
-account.balance = 1; //set
 
-// in total we do not need setter , because there is no reason to user be able to change the balance value
+let seats = new SeatAssignment()
+seats.A1 = 'mohammad';
+// seat['A1'] = "mohammad"
+seats.A2 = "Amir";
