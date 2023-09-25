5fe0ed51e7db66c3fc98dcd7d62fa6267d2091f7
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-09-25 10:22:01 +0100

    Using wildcard

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 //Import and Export
-import {Circle} from './shapes';
-let circle = new Circle(1)
-console.log(circle.radius);
+// import {Circle} from './shapes';
+// let circle = new Circle(1)
+// console.log(circle.radius);
+
+// export with default export
+// import Store,{Format} from "./storage";
 
 
-import Store,{Format} from "./storage";
+wildcard => when we wanna import lots of items 
+import {Circle, Square} from './shapes';
+import * as shapes from './shapes';
+shapes.
+
+
+
+
+
+
 
 
 
