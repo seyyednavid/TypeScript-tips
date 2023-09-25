dfef60dee9f70adb56043e89724df96bdfeafaed
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-09-25 21:58:54 +0100

    import js file into ts file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-//ReExporting
-// import {Circle} from './shapes/Circle';
-// import {Square} from './shapes/Square';
-
-// sometimes we have lots of shapes , so it is not a good solution
-// go to shapes folder, change shape.ts file to index.ts
-//Add shapes into index.ts in shapes folder and again export them
-import {Circle,Square} from './shapes/index'
-
-
-
-
-
-
-
-
-
-
-
-
+import { calculateTax } from "./tax";
+let tax = calculateTax(100);
+console.log(tax);
